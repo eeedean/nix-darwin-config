e281642c787ca714484c8d1582f1bf4a70238d49
Dean Eckert <dean.eckert@red-oak-consulting.com>
2024-09-17 14:16:54 +0200

    MBP-von-Dean: fixed Alias creation for Home Manager-/Nix-installed Apps

--- hosts/darwin/home.nix
+++ hosts/darwin/home.nix
@@ -59,23 +59,15 @@
       # This should be removed once
       # https://github.com/nix-community/home-manager/issues/1341 is closed.
       aliasApplications = lib.hm.dag.entryAfter ["writeBoundary"] ''
-        #set -o xtrace
-        app_folder="$(echo ~/Applications)/Home Manager Apps"
-        home_manager_app_folder="$genProfilePath/home-path/Applications"
-        $DRY_RUN_CMD rm -rf "$app_folder"
-        # NB: aliasing ".../home-path/Applications" to "~/Applications/Home Manager Apps" doesn't
-        #     work (presumably because the individual apps are symlinked in that directory, not
-        #     aliased). So this makes "Home Manager Apps" a normal directory and then aliases each
-        #     application into there directly from its location in the nix store.
-        $DRY_RUN_CMD mkdir -p "$app_folder"
-        find "$newGenPath/home-path/Applications/" -maxdepth 1 -mindepth 1 | while read app;
-        do
-          appname=$(find "$app" -maxdepth 0 -printf "%f\n")
-          $DRY_RUN_CMD /usr/bin/osascript \
-            -e "tell app \"Finder\"" \
-            -e "make new alias file at POSIX file \"$app_folder\" to POSIX file \"$app\"" \
-            -e "set name of result to \"$appname\"" \
-            -e "end tell"
+	new_nix_apps="${config.home.homeDirectory}/Applications/Nix"
+        rm -rf "$new_nix_apps"
+        mkdir -p "$new_nix_apps"
+        find -H -L "$newGenPath/home-path/Applications" -name "*.app" -type d -print | while read -r app; do
+          real_app=$(readlink -f "$app")
+          app_name=$(basename "$app")
+          target_app="$new_nix_apps/$app_name"
+          echo "Alias '$real_app' to '$target_app'"
+          ${pkgs.mkalias}/bin/mkalias "$real_app" "$target_app"
         done
       '';
     };
